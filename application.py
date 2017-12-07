47216cb001bf29e2d07f3488d492ea4520fba3dd
Krishna Suraj <krishnasuraj.lhs@gmail.com>
2017-12-07 01:20:08 -0500

    idk

--- application.py
+++ application.py
@@ -1,4 +1,7 @@
 from cs50 import SQL
+import sys
+import spotipy
+import spotipy.util as util
 from flask import Flask, flash, redirect, render_template, request, session
 #from flask_sqlalchemy import SQLAlchemy
 from flask_session import Session
@@ -26,15 +29,26 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-db = SQL("sqlite:///finance.db")
+#db = SQL("sqlite:///finance.db")
 
 #initializing the authentication token to something
 auth = 0
 
-@app.route("/auth", methods = ["GET", "POST"])
-def auth_user():
+#@app.route("/auth", methods = ["GET", "POST"])
+#def auth_user():
 
-
+aggregate = {"danceability": 0.0,
+    "energy": 0.0,
+    "key": 0.0,
+    "loudness": 0.0,
+    "mode": 0.0,
+    "speechiness": 0.0,
+    "acousticness": 0.0,
+    "instrumentalness": 0.0,
+    "liveness": 0.0,
+    "valence": 0.0,
+    "tempo": 0.0
+}
 
 @app.route("/")
 @login_required
@@ -65,3 +79,15 @@ def logout():
 @app.route("/register", methods=["GET", "POST"])
 def register():
     return render_template("register.html")
+
+def score(songs):
+    for
+
+
+
+
+    url = "idk" #the get request for the
+    r = requests.get(url)
+    data=r.json()
+
+
